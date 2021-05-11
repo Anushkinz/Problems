f26669804471d3aa6594626f950f31dd54b1b5b2
Anushkinz <anuarprogramm@gmail.com>
2021-05-11 22:39:10 +0600

    Problems commit

---
+++ problems neobis.playground/Contents.swift
@@ -0,0 +1,32 @@
+//var n:Int = 3
+//var m:Int = 3
+//var i:Int = 0
+//var j:Int = 0
+//while(i<n){
+//    while (j<m) {
+//        if i % 2 == 0 {
+//          print("#")
+//        }else if (j==m && i % 4 == 2) {
+//          print("#")
+//        }else if (j==1 && i % 4 == 0){
+//            print("#")
+//        }else{print(".")}
+//        print("/n")
+//        j+=1
+//    }
+//    i+=1
+//}
+
+
+///problem Multiples of 3 and 5
+//var i:Int = 0
+//while (i<1000){
+//    if (i%3 == 0 || i%5 == 0){
+//        print(i)
+//    }
+//    i += 1
+//}
+
+
+
+/// problem
